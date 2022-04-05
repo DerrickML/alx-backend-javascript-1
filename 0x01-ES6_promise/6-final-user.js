90d6a54b75ae27dfaabd7c1f647487eb1dcc012f
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-04-05 18:26:04 +0300

    fix eslint error

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup (firstName ,lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const userData = await signUpUser(firstName, lastName).then((data) => ({
     status: 'fulfilled',
     value: data,
