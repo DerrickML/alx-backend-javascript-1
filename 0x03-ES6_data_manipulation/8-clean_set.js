460f62860874780440254081a405d25ed3d9dc4f
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-04-11 18:16:27 +0300

    fix task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || startString.length === 0) return '';
   if (typeof set !== 'object') return '';
+  if (!startString || typeof startString !== 'string' || startString.length === 0) return '';
 
   const values = Array.from(set);
   const array = [];
 
   for (const value of values) {
-    if (value.startsWith(startString)) {
+    if (value && value.startsWith(startString)) {
       array.push(value.replace(startString, ''));
     }
   }
