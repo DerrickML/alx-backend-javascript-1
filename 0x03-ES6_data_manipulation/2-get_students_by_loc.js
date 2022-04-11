3a8a1ded7fca94b4ad8a2297b1717a924b02f96a
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-04-11 18:03:47 +0300

    fix task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,3 @@
 export default function getStudentsByLocation(students, city) {
-  if (!Array.isArray(students)) return [];
-
-  if (students && !city) return students;
-
   return students.filter((student) => student.location === city);
 }
